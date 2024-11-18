846380fa51e369ab37d39efe2f51b8847cfe1029
Robin <robin_ov@yahoo.de>
2024-11-18 18:55:14 +0100

    gitignore for ros bag and small additions for occupancyNode

--- src/occupancy_map_pkg/occupancy_map_pkg/occupancy_node.py
+++ src/occupancy_map_pkg/occupancy_map_pkg/occupancy_node.py
@@ -3,10 +3,15 @@ import rclpy
 class OccupancyNode(Node):
     def __init__(self):
         super().__init__('occupancy_node')
+        self.map = []
 
 
 def main(args=None):
     rclpy.init(args=args)
+    node = OccupancyNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
