bac4371e04bbf309e0492d4dd13536b2f8002ce3
Banujan Baskaran <banujan.baskaran@ruhr-uni-bochum.de>
2025-03-05 13:14:14 +0100

    (not working) updated cone detection

--- src/cone_detection_pkg/cone_detection_pkg/cone_detection_node.py
+++ src/cone_detection_pkg/cone_detection_pkg/cone_detection_node.py
@@ -15,12 +15,16 @@ from pathlib import Path
 class ConeDetectionNode(Node):
     def __init__(self):
         super().__init__('cone_detection_node')
-        self.model = YOLO(Path(__file__).parent / "ml_model"/"best_50_synthetic.pt")
+        self.model = YOLO(Path(__file__).parent / "ml_model"/"best_l.pt")
         self.bridge = CvBridge()
 
         # Subscribers
-        self.color_sub = Subscriber(self, Image, '/camera/color/image_raw')
-        self.depth_sub = Subscriber(self, Image, '/camera/aligned_depth_to_color/image_raw')
+        self.color_sub = self.create_subscription(
+            Image,
+            '/camera/color/image_raw',
+            self.image_callback,
+            10
+        )
 
         # thresholds for box
         # TODO: specify values
@@ -31,17 +35,15 @@ class ConeDetectionNode(Node):
 
         self.subscription_toggle_camera = self.create_subscription(
             Bool,
-            '/toggle_camera', self.toggle_camera_callback, 10)
+            '/toggle_camera', 
+            self.toggle_camera_callback, 
+            10
+        )
         self.toggle_camera = True
 
         # Publisher
         self.publisher_detections = self.create_publisher(DetectionArrayStamped, "detections", 10)
         self.publisher_detections_images = self.create_publisher(Image, "detections/images", 10)
-
-        # Synchronize color and depth topics
-        self.ts = ApproximateTimeSynchronizer(
-            [self.color_sub, self.depth_sub], queue_size=10, slop=0.1)
-        self.ts.registerCallback(self.image_callback)
 
         self.get_logger().info("Cone Detection Node has been started.")
 
@@ -51,27 +53,25 @@ class ConeDetectionNode(Node):
 
     def image_callback(self, color_msg):
         try:
-            # Convert ROS images to OpenCV format
+            # Convert ROS image to OpenCV format
             color_image = self.bridge.imgmsg_to_cv2(color_msg, desired_encoding='bgr8')
         except Exception as e:
-            self.get_logger().error('Failed to convert images: %s' % str(e))
+            self.get_logger().error('Failed to convert image: %s' % str(e))
             return
 
         color_image_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
 
         # Perform object detection
-        results = self.model.predict(color_image_rgb, save=False, show=False, device="cuda")
+        results = self.model.predict(color_image_rgb, save=False, show=False)
 
         # Initialize DetectionArray message
         detection_array = DetectionArray()
-
 
         # Draw bounding boxes and labels on the image
         for result in results:
             boxes = result.boxes.xyxy.cpu().numpy()
             confidences = result.boxes.conf.cpu().numpy()
             labels = result.boxes.cls.cpu().numpy()
-
 
             for box, conf, label in zip(boxes, confidences, labels):
                 x1, y1, x2, y2 = box
@@ -81,69 +81,12 @@ class ConeDetectionNode(Node):
                 box_height = y2 - y1
 
                 # filter out bounding boxes
-                # if box_width < self.min_width or box_height < self.min_height or box_width > self.max_width or box_height > self.max_height:
-                #    continue
-
-                # colors=[(255,0,0), (0,209,134), (0,255,255)]
-
-                # Draw bounding box
-                # cv2.rectangle(color_image_rgb, (int(x1), int(y1)), (int(x2), int(y2)), colors[int(label)], 2)
-
-                # Calculate center of bounding box
-                # x_center = int((x1 + x2) / 2)
-                # y_center = int((y1 + y2) / 2)
-
-                # Check if center coordinates are within the image
-                # if 0 <= x_center < width and 0 <= y_center < height:
-                #     # Retrieve depth value at center point
-                #     z_value = color_image_rgb[y_center, x_center]
-
-                #     # Check if depth value is valid
-                #     if np.isfinite(z_value) and z_value > 0:
-                #         # Convert to meters if necessary (assuming depth in millimeters)
-                #         z_in_meters = z_value * 0.001  # If depth values are in millimeters
-                #         z_text = f"Z: {z_in_meters:.2f} m"
-                #     else:
-                #         #continue
-                #         z_in_meters = float('nan')
-                #         z_text = "Z: N/A"
-                # else:
-                #     #continue
-                #     z_in_meters = float('nan')
-                #     z_text = "Z: Out of bounds"
-
-                # Draw center point on the image
-                # cv2.circle(color_image_rgb, (x_center, y_center), radius=5, color=(0, 255, 0), thickness=-1)
-
-                # Add depth value to the image
-                # cv2.putText(
-                #     color_image_rgb,
-                #     z_text,
-                #     (x_center + 10, y_center),
-                #     cv2.FONT_HERSHEY_SIMPLEX,
-                #     0.5,
-                #     colors[int(label)],
-                #     2,
-                # )
-
-                # # Add label and confidence score
-                # cv2.putText(
-                #     color_image_rgb,
-                #     f"Class {int(label)}: {conf:.2f}",
-                #     (int(x1), int(y1) - 10),
-                #     cv2.FONT_HERSHEY_SIMPLEX,
-                #     0.5,
-                #     colors[int(label)],
-                #     2,
-                # )
+                if box_width < self.min_width or box_height < self.min_height or box_width > self.max_width or box_height > self.max_height:
+                   continue
 
                 # Create Detection message
                 detection = Detection()
-                # detection.z_in_meters = float(z_in_meters)
-                # detection.angle = float(self.get_angle(x_center))
                 detection.label = int(label)
-
-                # self.get_logger().info(f"Detection: classID={detection.label}, angle={detection.angle}, distance={detection.z_in_meters}, x_center={x_center}, y_center={y_center}")
 
                 # Append detection to DetectionArray
                 detection_array.detections.append(detection)
@@ -153,32 +96,17 @@ class ConeDetectionNode(Node):
         detection_array_stamped.header = color_msg.header
         detection_array_stamped.detectionarray = detection_array
         self.publisher_detections.publish(detection_array_stamped)
-
-        # Optionally, save the image using cvbridge
-
+        # TODO: HIER IRGENDWAS IST KAPUTT
+        # WIR HABEN ES ALS BGR UND GEBEN BEI ANNOTATED_IMAGE_MSG AN DAS ANNOTATED IMAGE MSG ALS RGB8 ENCODING HAT
+        # RESIZE KÖNNEN WIR AUCH IM CAMERA NODE MACHEN? 
         # Convert RGB image back to BGR for saving
-        save_image = color_image_rgb#cv2.cvtColor(color_image_rgb, cv2.COLOR_RGB2BGR)
+        save_image = cv2.cvtColor(color_image_rgb, cv2.COLOR_RGB2BGR)
         save_image = cv2.resize(save_image, (640, 480))
         # Convert the annotated image to a ROS Image message
         annotated_image_msg = self.bridge.cv2_to_imgmsg(save_image, encoding='rgb8')
         annotated_image_msg.header = color_msg.header
         if self.toggle_camera:
             self.publisher_detections_images.publish(annotated_image_msg)
-
-        # Save the annotated image
-        # timestamp = color_msg.header.stamp.sec  # Use ROS message timestamp
-        #output_path = f"/workspace/src/cone_detection_pkg/cone_detection_pkg/image/detections_{time.time}.png"  # Change the path as needed
-        #self.publisher_detections_images.publish(save_image)
-        #cv2.imwrite(output_path, save_image)
-        #self.get_logger().info(f"Image saved: {output_path}")
-
-    # def get_angle(self, x_center):
-    #     CAMERA_RGB_FOV = 69  # degrees
-    #     CAMERA_RGB_PIXEL_WIDTH = 1280  # pixels
-    #     angle = (x_center - (CAMERA_RGB_PIXEL_WIDTH / 2)) * (CAMERA_RGB_FOV / CAMERA_RGB_PIXEL_WIDTH)
-    #     return angle
-
-
 
 def main(args=None):
     rclpy.init(args=args)
@@ -190,7 +118,6 @@ def main(args=None):
     finally:
         node.destroy_node()
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
